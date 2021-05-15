b5533332a5c0c2a3e0a17f905b82ee41eba85bef
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-16 02:12:04 +0900

    초기 커밋.

---
+++ ConsoleApplication44/ConsoleApplication44.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main() {
+	char str[64] = "this is test string for char pointer";
+	char* p = str;
+
+	while (1)
+	{
+		if (islower(p[0]));
+		p[0] = toupper(p[0]);
+
+		p = strchr(p, ' ');
+		if (p == NULL)
+			break;
+		p++;
+	}
+	puts(str);
+	printf("%s", str);
+}
